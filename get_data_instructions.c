9fb8343f0fc3f2cf9be5ddd06397344beb634732
Regis AMICHIA <ramichia@e2r8p14.42.fr>
2017-04-30 16:19:32 +0200

    function v3

--- get_data_instructions.c
+++ get_data_instructions.c
@@ -6,7 +6,7 @@
 /*   By: ramichia <ramichia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 16:00:50 by ramichia          #+#    #+#             */
-/*   Updated: 2017/04/30 15:06:23 by ramichia         ###   ########.fr       */
+/*   Updated: 2017/04/30 16:19:12 by ramichia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,16 +32,6 @@ void 	*get_pc(t_proc *processus, t_corewar *corewar)
 	return(adr);
 }
 
-void 	live(t_proc *processus, t_corewar *corewar)
-{
-	t_champ	*last_champ;
-
-	processus->live++;
-	last_champ = get_player(corewar, processus);
-	corewar->last_alive = last_champ->number;
-	processus->pc += 5;
-}
-
 void 	c_and(t_proc *processus, t_corewar *corewar)
 {
 	char			*p1;
@@ -54,7 +44,7 @@ void 	c_and(t_proc *processus, t_corewar *corewar)
 	processus->pc++;
 	p1 = get_value(processus, corewar, REG_SIZE, tab[0]);
 	p2 = get_value(processus, corewar,REG_SIZE, tab[1]);
-	tmp = get_adr_reg(processus, corewar);
+	tmp = (unsigned int*)get_adr_reg(processus, corewar);
 	*tmp = (unsigned int)p1 & (unsigned int)p2;
 	modify_carry(processus);
 	processus->pc++;
@@ -72,7 +62,7 @@ void 	c_or(t_proc *processus, t_corewar *corewar)
 	processus->pc++;
 	p1 = get_value(processus, corewar, REG_SIZE, tab[0]);
 	p2 = get_value(processus, corewar,REG_SIZE, tab[1]);
-	tmp = get_adr_reg(processus, corewar);
+	tmp = (unsigned int*)get_adr_reg(processus, corewar);
 	*tmp = (unsigned int)p1 | (unsigned int)p2;
 	modify_carry(processus);
 	processus->pc++;
@@ -90,7 +80,7 @@ void 	c_xor(t_proc *processus, t_corewar *corewar)
 	processus->pc++;
 	p1 = get_value(processus, corewar, REG_SIZE, tab[0]);
 	p2 = get_value(processus, corewar,REG_SIZE, tab[1]);
-	tmp = get_adr_reg(processus, corewar);
+	tmp = (unsigned int*)get_adr_reg(processus, corewar);
 	*tmp = (unsigned int)p1 ^ (unsigned int)p2;
 	modify_carry(processus);
 	processus->pc++;
@@ -99,14 +89,14 @@ void 	c_xor(t_proc *processus, t_corewar *corewar)
 void 	ld(t_proc *processus, t_corewar *corewar)
 {
 	char	*p1;
-	void 	*tmp;
+	int 	*tmp;
 	int		*tab;
 
 	processus->pc++;
 	tab = byte_analysis(processus, corewar);
 	p1 = get_value(processus, corewar, REG_SIZE, tab[0]);
-	tmp = get_adr_reg(processus, corewar);
-	tmp = p1;
+	tmp = (int*)get_adr_reg(processus, corewar);
+	*tmp = (int)p1;
 	modify_carry(processus);
 	processus->pc++;
 }
@@ -114,14 +104,14 @@ void 	ld(t_proc *processus, t_corewar *corewar)
 void 	lld(t_proc *processus, t_corewar *corewar)
 {
 	char	*p1;
-	void 	*tmp;
+	int 	*tmp;
 	int		*tab;
 
 	processus->pc++;
 	tab = byte_analysis(processus, corewar);
 	p1 = get_value_nm(processus, corewar, REG_SIZE, tab[0]);
-	tmp = get_adr_reg(processus, corewar);
-	tmp = p1;
+	tmp = (int*)get_adr_reg(processus, corewar);
+	*tmp = (int)p1;
 	modify_carry(processus);
 	processus->pc++;
 }
@@ -152,8 +142,8 @@ void 	lldi(t_proc *processus, t_corewar *corewar)
 
 	processus->pc++;
 	tab = byte_analysis(processus, corewar);
-	p1 = get_value(processus, corewar, IND_SIZE, tab[0]);
-	nbr = (int)p1 + (int)get_value(processus, corewar, REG_SIZE, tab[1]);
+	p1 = get_value_nm(processus, corewar, IND_SIZE, tab[0]);
+	nbr = (int)p1 + (int)get_value_nm(processus, corewar, REG_SIZE, tab[1]);
 	tmp = get_adr_reg(processus, corewar);
 	ft_memcpy(tmp, get_pc(processus, corewar) + nbr , REG_SIZE);
 	modify_carry(processus);
@@ -175,62 +165,65 @@ void 	st(t_proc *processus, t_corewar *corewar)
 
 void 	sti(t_proc *processus, t_corewar *corewar)
 {
-	int		*tab;
-	void 	*tmp;
-	char	*p1;
-	char 	*p2;
-	char 	*p3;
+	int				*tab;
+	unsigned int 	*tmp;
+	char			*p1;
+	char 			*p2;
+	char 			*p3;
 
 	processus->pc++;
 	tab = byte_analysis(processus, corewar);
 	p1 = get_reg_value(processus, corewar);
 	p2 = get_value(processus, corewar, REG_SIZE, tab[1]);
 	p3 = get_value(processus, corewar, REG_SIZE, tab[2]);
-	tmp = corewar->memory + (unsigned int)p2 + (unsigned int)p3;
-	tmp = p1;
+	if ((unsigned int)p2 + (unsigned int)p3 < MEM_SIZE)
+		tmp = (unsigned int*)(corewar->memory + (unsigned int)p2 + (unsigned int)p3);
+	else
+		tmp = (unsigned int*)corewar->memory;
+	*tmp = (unsigned int)p1;
 	processus->pc++;
 }
 
 void 	add(t_proc *processus, t_corewar *corewar)
 {
-	unsigned int	*p3;
-	char 			*p1;
-	char 			*p2;
-
+	int		*p3;
+	char 	*p1;
+	char 	*p2;
 
 	processus->pc+= 2;
 	p1 = get_reg_value(processus, corewar);
 	p2 = get_reg_value(processus, corewar);
-	p3 = get_adr_reg(processus, corewar);
+	p3 = (int*)get_adr_reg(processus, corewar);
 	*p3 = (unsigned int)p1 + (unsigned int)p2;
 	modify_carry(processus);
 }
 
 void 	sub(t_proc *processus, t_corewar *corewar)
 {
-	unsigned int	*p3;
-	char 			*p1;
-	char 			*p2;
-
+	int		*p3;
+	char 	*p1;
+	char 	*p2;
 
 	processus->pc+= 2;
 	p1 = get_reg_value(processus, corewar);
 	p2 = get_reg_value(processus, corewar);
-	p3 = get_adr_reg(processus, corewar);
+	p3 = (int*)get_adr_reg(processus, corewar);
 	*p3 = (unsigned int)p1 - (unsigned int)p2;
 	modify_carry(processus);
 }
 
 void 	zjmp(t_proc *processus, t_corewar *corewar)
 {
-	void 			*tmp;
-	unsigned int 	p1;
+	void 	*tmp;
+	int 	p1;
 
 	processus->pc++;
 	tmp = get_pc(processus, corewar);
-	p1 = (unsigned int)tmp;
+	p1 = (int)tmp;
 	if (processus->carry == 1)
 		processus->pc = processus->pc + (p1 % IDX_MOD);
+	if ((int)processus->pc < 0 || MEM_SIZE < processus->pc)
+		processus->pc = 0;
 }
 
 void 	c_fork(t_proc *processus, t_corewar *corewar)
@@ -261,21 +254,17 @@ void 	lfork(t_proc *processus, t_corewar *corewar)
 	processus->pc++;
 	tmp = get_pc(processus, corewar);
 	p1 = (unsigned int)tmp;
-	processus2->pc = processus->pc + p1 ;
+	processus2->pc = processus->pc + p1;
 	if (MEM_SIZE < processus2->pc)
 		processus2->pc = 0;
 	ft_parrpush((void***)&corewar->proc, processus2);
 }
 
-// void 	aff(t_proc *processus, t_corewar *corewar)
-// {
-// 	void 	*tmp;
-// 	int		mv;
-// 	char 	*p1;
-//
-// 	mv = 0;
-// 	tmp = processus->pc;
-// 	tmp = move_adr(tmp, corewar, 2, &mv);
-// 	p1 = get_value(tmp, &mv);
-// 	tmp = processus->pc + mv;
-// }
+void 	aff(t_proc *processus, t_corewar *corewar)
+{
+	char	aff;
+
+	processus->pc += 2;
+	aff = (char)get_reg_value(processus, corewar);
+	ft_putchar(aff);
+}
