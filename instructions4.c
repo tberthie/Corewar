7624f205028eb55213a773dd0225d6eea1bab1e2
Regis AMICHIA <ramichia@e2r7p13.42.fr>
2017-05-03 16:34:38 +0200

    zjmp norme

--- instructions4.c
+++ instructions4.c
@@ -6,7 +6,7 @@
 /*   By: ramichia <ramichia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/02 19:31:07 by ramichia          #+#    #+#             */
-/*   Updated: 2017/05/03 16:29:06 by ramichia         ###   ########.fr       */
+/*   Updated: 2017/05/03 16:33:44 by ramichia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@ void	zjmp(t_proc *processus, t_corewar *corewar, unsigned char op)
 {
 	int		p1;
 	int		tmp;
+	void	*adr;
+	void	*adr2;
 
-	processus->pc++;
 	tmp = processus->pc;
-	p1 = *(char*)tmp << 8 + *(char*)++tmp;
+	processus->pc++;
+	adr = get_pc(processus, corewar);
+	processus->pc++;
+	adr2 = get_pc(processus, corewar);
+	p1 = *(char*)adr << 8 + *(char*)adr2;
 	if (processus->carry == 1)
 	{
 		tmp = (tmp + (p1 % IDX_MOD));
